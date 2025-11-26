b6a79e56244e463ebb1fcd1302d6bad0528926b3
Marcy_ <marciagisseli21@gmail.com>
2025-11-26 11:04:51 -0300

    :rocket: Deploy do Projeto Projeto Blog Pessoal

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -8,9 +8,12 @@ function Footer() {
         let data = new Date().getFullYear()
 
         const{usuario}= useContext(AuthContext);
+        
         let component: ReactNode
 
         if(usuario.token !== ""){
+            component=(
+
             <div className="flex justify-center bg-indigo-900 text-white">
                 <div className="container flex flex-col items-center py-4">
                     <p className='text-xl font-bold'>
@@ -24,6 +27,7 @@ function Footer() {
                     </div>
                 </div>
             </div>
+            )
         }
 
     return (
